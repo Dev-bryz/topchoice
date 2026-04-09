fba4541d51062e1187203af6db7d7dfa1f9de5e2
Dev-bryz <lihongfu19@gmail.com>
2026-04-09 04:35:19 +0000

    move screen_1 to public

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
     siteName: "Topchoice Forwarders",
     images: [
       {
-        url: "/assets/screen_1.png", // put this in /public
+        url: "/screen_1.png", // put this in /public
         width: 1200,
         height: 630,
         alt: "Topchoice Forwarders",
